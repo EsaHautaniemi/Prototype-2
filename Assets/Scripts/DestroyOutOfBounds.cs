2e325029ee6a0204b8fa8b03e176af9c7cae5f82
Esa Hautaniemi <esa.hautaniemi@outlook.com>
2020-04-07 13:22:24 +0300

    Prototype 2

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+
+    private float topBound = 30f;
+    private float lowerBound = -10f;
+
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Game over!");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] animalPrefabs;
+    [SerializeField] float spawnRangeX = 20;
+    [SerializeField] float spawnPosZ = 20;
+    [SerializeField] float startDelay = 2;
+    [SerializeField] float spawnInterval = 1.5f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+    private void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }
+}
